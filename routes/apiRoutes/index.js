ff0d0f421ba6c5957b4fea4739408dd0a90bdb06
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2022-02-14 03:17:25 -0500

    updates

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const voterRoutes = require('./voterRoutes')
+const voteRoutes = require('./voteRoutes')
 
 router.use('/', voterRoutes);
+router.use('/', voteRoutes);
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
+
 
 
 module.exports = router;
